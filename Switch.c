b7cd4f734a601996e788e36c6166c33d40e95930
dipanshisolanki <72515567+dipanshisolanki@users.noreply.github.com>
2020-10-10 12:10:39 +0530

    Switch.c

---
+++ Switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+printf("food item: \n1. Pizza\n2. Burger\n3. Pasta\n4. French fries\n5. Sandwich");
+scanf("%d",&item)
+int item=0;
+Switch (item)
+{
+Case1:printf("price-Rs239");
+Break;
+Case2:printf("price-Rs129");
+Break;
+Case3:printf("price-Rs179");
+Break;
+Case4:printf("price-Rs99");
+Break;
+Case5:printf("price-Rs149");
+Break;
+Default:printf("we have limited items");
+}
